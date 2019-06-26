e1fd20a6de189fa371c67c6f5ec5eb0ab2864c60
Alexey Pokrovskiy <dj_alexeyka@mail.ru>
2019-06-26 05:14:49 +0300

    change namespace ...\Models\...

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+// Users
+Route::resource('/user','UserController');
+// Section
+Route::resource('/section','SectionController');
